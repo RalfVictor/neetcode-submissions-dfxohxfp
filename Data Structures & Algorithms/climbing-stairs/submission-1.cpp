b6217236827a3dc014ef46450acffab344422d5f
Ralf_Victor <142431936+RalfVictor@users.noreply.github.com>
2026-04-20 11:19:44 +0530

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int dfs(int i,int n,vector<int>& dp){
+        if(i>=n) return i==n;
+        if(dp[i]!=-1) return dp[i];
+        return dp[i]=dfs(i+1,n,dp)+dfs(i+2,n,dp);
+    }
+    int climbStairs(int n) {
+        vector<int> dp(n,-1);
+        return dfs(0,n,dp);
+    }
+};
